d2a000abea9f9f59365f934e998d899ef509a1d1
Ahmad Alkashami <a.alkashami@zkood.com>
2020-09-16 15:33:26 +0300

    Form

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form1 from './Forms/Form1'
+import Form2 from './Forms/Form2'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form1 />
+      <Form2 />
     </div>
-  );
+  )
 }
-
 export default App;
